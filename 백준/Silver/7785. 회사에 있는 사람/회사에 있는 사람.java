b4cb95e54011cbbdae97b4b77d748d4add3dac65
LeeSH <161822292+LLSNsssz@users.noreply.github.com>
2024-11-03 22:30:10 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 728 ms, Memory: 58856 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,35 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        Set<String> presentEmployees = new HashSet<>();
+        // HashMap을 사용하여 현재 회사에 있는 사람들을 추적
+        HashSet<String> company = new HashSet<>();
         
+        // 각 로그 처리
         for (int i = 0; i < n; i++) {
             String[] log = br.readLine().split(" ");
             String name = log[0];
             String action = log[1];
             
             if (action.equals("enter")) {
-                presentEmployees.add(name);
-            } else if (action.equals("leave")) {
-                presentEmployees.remove(name);
+                company.add(name);
+            } else { // leave
+                company.remove(name);
             }
         }
         
-        List<String> sortedEmployees = new ArrayList<>(presentEmployees);
-        Collections.sort(sortedEmployees, Collections.reverseOrder());
+        // 현재 회사에 있는 사람들을 리스트로 변환하고 정렬
+        ArrayList<String> result = new ArrayList<>(company);
+        Collections.sort(result, Collections.reverseOrder());
         
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (String employee : sortedEmployees) {
-            bw.write(employee + "\n");
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (String name : result) {
+            sb.append(name).append("\n");
         }
         
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
         br.close();
     }
 }
